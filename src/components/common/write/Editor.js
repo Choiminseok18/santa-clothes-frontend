e3f2f7f2d73755defb48da9bff9d592b673ee1aa
MinSeok Choi <100674871+Choiminseok18@users.noreply.github.com>
2024-01-09 14:33:37 +0900

    fix: vercel 배포 오류 수정

--- src/components/common/write/Editor.js
+++ src/components/common/write/Editor.js
@@ -1,6 +1,5 @@
 import { useRef, useEffect } from 'react';
 import Quill from 'quill';
-import 'quill/dist/quill.bubble.css';
 import styled from 'styled-components';
 import palette from '../../../lib/styles/palette';
 import Responsive from '../Responsive';
