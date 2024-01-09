44e33b1e3ae4cbaea88ca8f3c292fb3578444e4c
MinSeok Choi <100674871+Choiminseok18@users.noreply.github.com>
2024-01-09 14:37:16 +0900

    fix: vercel 배포 오류 수정

--- src/components/main/MainTemplate.js
+++ src/components/main/MainTemplate.js
@@ -5,7 +5,6 @@ import thirdImg from '../main/thirdImg.png';
 import palette from "../../lib/styles/palette";
 import {Link} from "react-router-dom";
 import SlideBlock from "../common/SlideBlock";
-import Todo from "../../Todo";
 
 const MainTemplateBlock = styled.div`
   position: relative;
